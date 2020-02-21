96452056f27d18ccfaf3a4393e648466fbc82b9e
Stephan Windmüller <windmueller@e-spirit.com>
2020-02-21 16:03:21 +0100

    Initial commit

---
+++ src/main/java/de/freewarepoint/issue20262demo/Issue20262DemoApplication.java
@@ -0,0 +1,13 @@
+package de.freewarepoint.issue20262demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Issue20262DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Issue20262DemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/freewarepoint/issue20262demo/Issue20262DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package de.freewarepoint.issue20262demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Issue20262DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
